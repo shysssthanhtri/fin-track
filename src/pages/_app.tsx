a601a3ae471b07cd3e45d6c2467236107e2571dd
Tri Nguyen <shyssthanhtri@gmail.com>
2024-10-13 18:38:49 +0700

    feat: init shadcn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,11 @@ import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 
 import { AuthGuard } from "@/components/guards/auth.guard";
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
 import { api } from "@/utils/api";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -14,11 +17,19 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <div className={GeistSans.className}>
-        <AuthGuard>
-          <Component {...pageProps} />
-        </AuthGuard>
-      </div>
+      <main className={cn(GeistSans.className, "font-sans antialiased")}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthGuard>
+            <Component {...pageProps} />
+          </AuthGuard>
+          <Toaster />
+        </ThemeProvider>
+      </main>
     </SessionProvider>
   );
 };
